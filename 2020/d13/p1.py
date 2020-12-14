24a8f6ab3866a5e38c99014b7234e9b6d674e532
Jeremy Satterfield <jeremy.satterfield@onepeloton.com>
2020-12-14 21:39:27 +0000

    add 2020 d13 p2

--- 2020/d13/p1.py
+++ 2020/d13/p1.py
@@ -1,3 +1,5 @@
+from math import prod
+
 FILENAME = 'test_input.txt'
 FILENAME = 'input.txt'
 
@@ -18,3 +20,18 @@ def p1():
             if cur_time % bus == 0:
                 return bus * ticks
         ticks += 1
+
+
+def calc_ctr(bus, rem, buses):
+    ni = prod([x[0] for x in buses if x[0] != bus])
+    x = 0
+    for x in range(bus):
+        if (x * ni) % bus == 1:
+            break
+    return (-rem % bus) * ni * x
+
+
+def p2():
+    _, buses = read_input()
+    buses = [(int(b),  i) for i, b in enumerate(buses.split(',')) if b != 'x']
+    return sum([calc_ctr(b[0], b[1], buses) for b in buses]) % prod([b[0] for b in buses])
